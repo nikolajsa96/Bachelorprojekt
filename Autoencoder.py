dc83bd47667681938b61ad9cd2a4e223b76f056c
nikolajsa96 <lrg407@alumni.ku.dk>
2021-04-05 15:01:23 +0200

    Autoboii

--- Autoencoder.py
+++ Autoencoder.py
@@ -10,7 +10,6 @@ import torch.nn.functional as F
 from torchvision import datasets
 from torch.utils.data import DataLoader
 from torchvision.utils import save_image
-# constants
 
 # constants
 NUM_EPOCHS = 10
@@ -68,24 +67,42 @@ class Autoencoder(nn.Module):
         self.enc3 = nn.Linear(in_features=128, out_features=64)
         self.enc4 = nn.Linear(in_features=64, out_features=32)
         self.enc5 = nn.Linear(in_features=32, out_features=16)
+        """
+        self.enc6 = nn.Linear(in_features=16, out_features=8)
+        self.enc7 = nn.Linear(in_features=8, out_features=4)
+        self.enc8 = nn.Linear(in_features=4, out_features=2)
         # decoder
-        self.dec1 = nn.Linear(in_features=16, out_features=32)
-        self.dec2 = nn.Linear(in_features=32, out_features=64)
-        self.dec3 = nn.Linear(in_features=64, out_features=128)
-        self.dec4 = nn.Linear(in_features=128, out_features=256)
-        self.dec5 = nn.Linear(in_features=256, out_features=784)
+        self.dec1 = nn.Linear(in_features=2, out_features=4)
+        self.dec2 = nn.Linear(in_features=4, out_features=8)
+        self.dec3 = nn.Linear(in_features=8, out_features=16)
+        """
+        self.dec4 = nn.Linear(in_features=16, out_features=32)
+        self.dec5 = nn.Linear(in_features=32, out_features=64)
+        self.dec6 = nn.Linear(in_features=64, out_features=128)
+        self.dec7 = nn.Linear(in_features=128, out_features=256)
+        self.dec8 = nn.Linear(in_features=256, out_features=784)
     def forward(self, x):
         x = F.relu(self.enc1(x))
         x = F.relu(self.enc2(x))
         x = F.relu(self.enc3(x))
         x = F.relu(self.enc4(x))
         x = F.relu(self.enc5(x))
+        """
+        x = F.relu(self.enc6(x))
+        x = F.relu(self.enc7(x))
+        x = F.relu(self.enc8(x))
+        """
         z = x
+        """
         x = F.relu(self.dec1(x))
         x = F.relu(self.dec2(x))
         x = F.relu(self.dec3(x))
+        """
         x = F.relu(self.dec4(x))
         x = F.relu(self.dec5(x))
+        x = F.relu(self.dec6(x))
+        x = F.relu(self.dec7(x))
+        x = F.relu(self.dec8(x))
         return x, z
 net = Autoencoder()
 print(net)
@@ -96,6 +113,7 @@ optimizer = optim.Adam(net.parameters(), lr=LEARNING_RATE)
 
 def train(net, trainloader, NUM_EPOCHS):
     train_loss = []
+    dimi_list = []
     for epoch in range(NUM_EPOCHS):
         running_loss = 0.0
         for data in trainloader:
@@ -104,6 +122,8 @@ def train(net, trainloader, NUM_EPOCHS):
             img = img.view(img.size(0), -1)
             optimizer.zero_grad()
             outputs = net(img)
+            outputs = outputs[0]
+            dimi = outputs[1]
             loss = criterion(outputs, img)
             loss.backward()
             optimizer.step()
@@ -111,11 +131,12 @@ def train(net, trainloader, NUM_EPOCHS):
 
         loss = running_loss / len(trainloader)
         train_loss.append(loss)
+        dimi_list.append(dimi)
         print('Epoch {} of {}, Train Loss: {:.3f}'.format(
             epoch + 1, NUM_EPOCHS, loss))
         if epoch % 5 == 0:
             save_decoded_image(outputs.cpu().data, epoch)
-    return train_loss
+    return train_loss, dimi_list
 
 
 def test_image_reconstruction(net, testloader):
@@ -124,6 +145,7 @@ def test_image_reconstruction(net, testloader):
         img = img.to(device)
         img = img.view(img.size(0), -1)
         outputs = net(img)
+        outputs = outputs[0]
         outputs = outputs.view(outputs.size(0), 1, 28, 28).cpu().data
         save_image(outputs, 'fashionmnist_reconstruction.png')
         break
@@ -137,12 +159,20 @@ make_dir()
 # train the network
 train_loss = train(net, trainloader, NUM_EPOCHS)
 plt.figure()
-plt.plot(train_loss)
+plt.plot(train_loss[0])
 plt.title('Train Loss')
 plt.xlabel('Epochs')
 plt.ylabel('Loss')
 plt.savefig('deep_ae_fashionmnist_loss.png')
 # test the network
 test_image_reconstruction(net, testloader)
+print(train_loss[1])
 
-
+"""
+plt.figure()
+plt.plot(z_list)
+plt.title('2D Boii')
+#plt.xlabel('Epochs')
+#plt.ylabel('Loss')
+plt.savefig('2D_Boii.png')
+"""
